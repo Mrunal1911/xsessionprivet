3651e3c39c3512bbb3243cd36392fabc030e0e89
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-08-24 21:45:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/Mrunal1911/xsessionprivet")
+os.system("git clone https://github.com/Mrunal1911/xsessionprivet.git")
 os.system("cd SessionHackkbot && python bot.py")
